c55bafc3bc2e7d4a8695e616ef6ca61410e4de3c
Lev <for.inst.techies@mail.ru>
2025-07-07 20:07:00 +0300

    Player update
    Took a break and added attack for player (J/LMB)

--- simpleplatformer/scripts/elements/player/player.gd
+++ simpleplatformer/scripts/elements/player/player.gd
@@ -10,82 +10,103 @@ enum PlayerStateMachine{
 	DEATH
 }
 
-
+# constants
 const VELOCITY_Y_MIN: float = -500
 const VELOCITY_Y_MAX: float = 500
 const SPEED: float = 15000.0
 const JUMP_SPEED: float = -25000.0
+const ATTACK_PUSH_SPEED: float = 14000
 
-
+# onready nodes
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var coyot_jump_timer: Timer = $CoyotJumpTimer
+@onready var damage_box: Node2D = $DamageBox
 
-
+# onready variables
 @onready var state: PlayerStateMachine = PlayerStateMachine.MOVE
 @onready var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+# variables
 var direction: float = 0
 var jump_allowed: bool = true
 
 
 func _ready() -> void:
+	# connecting signals
 	coyot_jump_timer.timeout.connect(_on_coyot_jump_timer_timeout)
 
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-			state = PlayerStateMachine.JUMP
+	if Input.is_action_just_pressed("jump") and is_on_floor(): # check for jump
+		state = PlayerStateMachine.JUMP
+	# check if attack pressed
+	elif Input.is_action_just_pressed("attack") \
+		and state != PlayerStateMachine.ATTACK: # check if attacking
+			print(state)
+			state = PlayerStateMachine.ATTACK
 
 
 func _physics_process(delta: float) -> void:
-	match state:
+	match state: # managing current state
 		PlayerStateMachine.MOVE:
 			move_state(delta)
 		PlayerStateMachine.JUMP:
 			jump_state(delta)
 		PlayerStateMachine.ATTACK:
-			pass
+			attack_state(delta)
 		PlayerStateMachine.DAMAGED:
 			pass
 		PlayerStateMachine.DEATH:
 			pass
 	
-	if !is_on_floor():
-		if coyot_jump_timer.time_left == 0:
+	if !is_on_floor(): # basic gravity
+		if coyot_jump_timer.time_left == 0: # coyot jumpt for better UE
 			coyot_jump_timer.start()
 		velocity.y += clamp(gravity*delta, VELOCITY_Y_MIN, \
 											VELOCITY_Y_MAX)
 	
 	move_and_slide()
 
-
-func move_state(delta: float):
-	direction = Input.get_axis("left", "right")
+# move state
+func move_state(delta: float): 
+	direction = Input.get_axis("left", "right") # getting direction for velocity
+												# and better animation
 	
-	if direction != 0:
-		set_look(direction)
-		if is_on_floor():
+	if direction != 0: # if pressing move buttons (left/right)
+		set_look(direction) # set player's look direction
+		if is_on_floor(): # check for animation and jump variable
 			animation_player.play("move")
 			jump_allowed = true
-		velocity.x = lerp(velocity.x, SPEED*delta*direction, 0.8)
-	else:
-		if is_on_floor():
+		velocity.x = lerp(velocity.x, SPEED*delta*direction, 0.8) # basic speed
+																  # equation
+	else: # if standing still
+		if is_on_floor(): # check for animation and jump variable
 			animation_player.play("idle")
 			jump_allowed = true
-		velocity.x = lerp(velocity.x, 0.0, 0.4)
+		velocity.x = lerp(velocity.x, 0.0, 0.4) # basic speed nullifying equation
 
-
+# jump state
 func jump_state(delta: float):
-	jump_allowed = false
-	velocity.y = JUMP_SPEED*delta
+	jump_allowed = false # no more jumps while jumping
+	velocity.y = JUMP_SPEED*delta # basic y axis speed equation
 	animation_player.play("jump")
+	state = PlayerStateMachine.MOVE # returing back to MOVE state
+
+# attack state
+func attack_state(delta: float):
+	velocity.x = ATTACK_PUSH_SPEED*delta*animated_sprite_2d.scale.x/abs(animated_sprite_2d.scale.x)
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(self, "velocity:x", 0,\
+	 0.2).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	animation_player.play("attack")
+	await animation_player.animation_finished
 	state = PlayerStateMachine.MOVE
 
 
 func set_look(_direction):
 	animated_sprite_2d.scale.x = _direction
+	damage_box.scale.x = _direction
 
 
 func _on_coyot_jump_timer_timeout():
