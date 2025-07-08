44cf65679e957374d184c60976cc268a1b792e54
Lev <for.inst.techies@mail.ru>
2025-07-08 13:05:28 +0300

    Release
    Made a bit of tutorial in the beggining and exported the project for Windows.
    That's all.

--- simpleplatformer/scripts/scenes/game.gd
+++ simpleplatformer/scripts/scenes/game.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
 
-#func _ready() -> void:
-	#Signals.player_is_dead.connect(_on_player_is_dead)
-#
-#
-#func _on_player_is_dead():
-	#get_tree().change_scene_to_file("res://scenes/game.tscn")
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
